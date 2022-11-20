d430dab8ab818f0bf67440b49e8a61373cb053db
bae-sh <hyeon1293@gmail.com>
2022-11-20 22:02:53 +0900

    feat : printMap 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -7,7 +7,25 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(bridge, positon) {
+    const [upBridge, downBridge] = bridge;
+    print(
+      `[${upBridge
+        .map((answer, index) => {
+          if (index <= positon) return ` ${answer} `;
+          return '   ';
+        })
+        .join('|')}]\ns`,
+    );
+    print(
+      `[${downBridge
+        .map((answer, index) => {
+          if (index <= positon) return ` ${answer} `;
+          return '   ';
+        })
+        .join('|')}]\ns`,
+    );
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
