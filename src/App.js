ca5b70e280aa9dffec12c9d15243444783fb92dc
bae-sh <hyeon1293@gmail.com>
2022-11-20 22:14:38 +0900

    feat : game 인스턴스 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-class App {
-  play() {}
-}
+const BridgeGame = require('./BridgeGame');
+const InputView = require('./InputView');
 
+class App {
+  play() {
+    const game = new BridgeGame();
+    InputView.readBridgeSize(game);
+  }
+}
+new App().play();
 module.exports = App;
