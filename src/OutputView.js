7c6c1ecdc8194006dfe90c23eedd7bf85b96d8cf
bae-sh <hyeon1293@gmail.com>
2022-11-20 23:11:24 +0900

    feat : 성공시 케이스 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -10,7 +10,6 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap({ bridge, trace, positon }) {
-    console.log(bridge, trace, positon);
     const order = ['U', 'D'];
     bridge.forEach((currentBridge, orderIndex) => {
       print(
